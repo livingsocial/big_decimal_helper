9c93b5d5f3ea7e4a618f02244755a37324c1ad9e
Sam Livingston-Gray <geeksam@gmail.com>
2012-08-24 16:12:59 -0700

    Add (and spec) ActiveRecord macro

---
+++ lib/big_decimal_helper/has_big_decimal_field.rb
@@ -0,0 +1,16 @@
+module BigDecimalHelper
+  # Provides an ActiveRecord macro to coerce fields to BigDecimal
+  module HasBigDecimalField
+    def has_big_decimal_field(attr_name)
+      composed_of attr_name,
+        :class_name  => 'BigDecimal',
+        :mapping => [ [attr_name, :to_f] ],
+        :constructor => Proc.new {|x| x.to_bd },
+        :converter   => Proc.new {|x| x.to_bd }
+    end
+  end
+end
+
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.send :extend, BigDecimalHelper::HasBigDecimalField
+end
---
+++ spec/active_record_macro_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+unless defined?(ActiveRecord::Base)
+  module ActiveRecord
+    class Base
+    end
+  end
+end
+
+# Deliberately require the file again with a different path, now that the target class is defined
+require File.join(File.dirname(__FILE__), *%w[.. lib big_decimal_helper has_big_decimal_field])
+
+
+describe 'ActiveRecord mixin' do
+  it 'gets mixed in to ActiveRecord::Base' do
+    ActiveRecord::Base.should be_kind_of(BigDecimalHelper::HasBigDecimalField)
+  end
+
+  it 'adds a .has_big_decimal_field method that invokes .composed_of' do
+    ActiveRecord::Base.should_receive(:composed_of)
+    ActiveRecord::Base.class_eval do
+      has_big_decimal_field :foo
+    end
+  end
+end
