44f32f5f8d746a6f5fc8f62b5e471a5b665238c8
Sam Livingston-Gray <sam.livingstongray@livingsocial.com>
2013-08-27 12:16:01 -0700

    Reorganize specs for conversion protocol

--- spec/conversion_protocol_spec.rb
+++ spec/conversion_protocol_spec.rb
@@ -1,37 +1,40 @@
 require 'spec_helper'
 
 describe 'Conversion protocol' do
-  it "converts a string to a BigDecimal" do
-    '42'.to_bd.should == BigDecimal('42')
+  describe "for numeric types" do
+    it "converts a BigDecimal to a BigDecimal" do
+      foo = BigDecimal('42')
+      foo.to_bd.should == foo
+    end
+
+    it "converts an integer to a BigDecimal" do
+      42.to_bd.should == BigDecimal('42')
+    end
+
+    it "converts a float to a BigDecimal" do
+      1.23.to_bd.should == BigDecimal('1.23')
+    end
   end
 
-  it "converts a float to a BigDecimal" do
-    1.23.to_bd.should == BigDecimal('1.23')
-  end
+  describe "for strings" do
+    it "converts a string to a BigDecimal" do
+      '42'.to_bd.should == BigDecimal('42')
+    end
 
-  it "converts a BigDecimal to a BigDecimal" do
-    foo = BigDecimal('42')
-    foo.to_bd.should == foo
-  end
+    it "converts a string with commas to a BigDecimal" do
+      '42,000'.to_bd.should == BigDecimal('42000')
+    end
 
-  it "converts a BigDecimal to itself" do
-    foo = BigDecimal('42')
-    foo.to_bd.should be(foo)
-  end
+    it "converts a string with dollar signs to a BigDecimal" do
+      '$42,000'.to_bd.should == BigDecimal('42000')
+    end
 
-  it "converts a string with commas to a BigDecimal" do
-    '42,000'.to_bd.should == BigDecimal('42000')
-  end
+    it "doesn't drop decimals either" do
+      '$42,000.42'.to_bd.should == BigDecimal('42000.42')
+    end
 
-  it "converts a string with dollar signs to a BigDecimal" do
-    '$42,000'.to_bd.should == BigDecimal('42000')
-  end
-
-  it "doesn't drop decimals either" do
-    '$42,000.42'.to_bd.should == BigDecimal('42000.42')
-  end
-
-  it "groks negative numbers" do # oops
-    '-42,000.42'.to_bd.should == BigDecimal('-42000.42')
+    it "groks negative numbers" do # oops
+      '-42,000.42'.to_bd.should == BigDecimal('-42000.42')
+    end
   end
 end
