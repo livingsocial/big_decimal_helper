649b54eebd2cf089e70fb101c04bdc00192cbfed
Sam Livingston-Gray <sam.livingstongray@livingsocial.com>
2013-11-08 16:46:17 -0800

    Whitespace

--- lib/big_decimal_helper/has_big_decimal_field.rb
+++ lib/big_decimal_helper/has_big_decimal_field.rb
@@ -4,7 +4,7 @@ module BigDecimalHelper
     def has_big_decimal_field(attr_name)
       composed_of attr_name,
         :class_name  => 'BigDecimal',
-        :mapping => [ [attr_name, :to_f] ],
+        :mapping     => [ [attr_name, :to_f] ],
         :constructor => Proc.new {|x| x.to_bd },
         :converter   => Proc.new {|x| x.to_bd }
     end
